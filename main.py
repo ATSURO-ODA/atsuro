72bf053b57c49e4a8087b7afab28f5f12f65db9c
Atsuro Oda <adoponn.777@gmail.com>
2025-08-15 23:04:26 +0900

    modify game

--- main.py
+++ main.py
@@ -7,30 +7,40 @@ class RPS_AI:
     def get_ai_choice(self) -> str:
         return random.choice(self.CHOICE_LIST)
 
-    def play_game(self) -> None:
+    def game(self) -> Bool:
         playerscore = 0
         AIscore = 0
 
-        for _ in range(10):
-            player_choice = input("グー、チョキ、パーのいずれかを入力してください：")
-            ai_choice = self.get_ai_choice()
+        player_choice = input(
+            "グー、チョキ、パー(終了する場合はqと入力してください)のいずれかを入力してください："
+        )
+        ai_choice = self.get_ai_choice()
 
-            if player_choice == ai_choice:
-                print("あいこ")
+        if player_choice == "q":
+            print("終了します")
+            return False
 
-            elif (
-                (player_choice == "グー" and ai_choice == "チョキ")
-                or (player_choice == "チョキ" and ai_choice == "パー")
-                or (player_choice == "パー" and ai_choice == "グー")
-            ):
-                print("プレイヤーの勝ちです")
-                playerscore += 1
+        elif player_choice == ai_choice:
+            print("あいこ")
 
-            else:
-                print("AIの勝ちです")
-                AIscore += 1
+        elif (
+            (player_choice == "グー" and ai_choice == "チョキ")
+            or (player_choice == "チョキ" and ai_choice == "パー")
+            or (player_choice == "パー" and ai_choice == "グー")
+        ):
+            print("プレイヤーの勝ちです")
+            playerscore += 1
+
+        else:
+            print("AIの勝ちです")
+            AIscore += 1
 
         print(f"\n最終スコア - プレイヤー: {playerscore}, AI: {AIscore}")
+
+    def play_game(self) -> None:
+        for _ in range(10):
+            if not self.game():
+                break
 
 
 def main():
